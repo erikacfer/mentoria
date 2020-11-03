3d7e4f251bf7a358b80627f3e2ec713ea507d88f
Erika Fernandes <fernandes.ec@gmail.com>
2020-11-03 11:37:33 -0300

    Incluir lista de acertos

--- megasena/lib/resultado_super.rb
+++ megasena/lib/resultado_super.rb
@@ -5,16 +5,17 @@ require_relative '..\lib\jogada.rb'
 class ResultadoSuper
   def conferir_resultado(numeros_apostados, numeros_sorteados)
     count = 0
-
+    acertos = []
     Array(numeros_apostados).each do |numero|
       if Array(numeros_sorteados).include? (numero)
         count += 1
+        acertos.push(numero)
       end
     end
     if count >= 1
-      puts "Parabéns, meu consagrado! Agora você venceu na vida."
+      puts "Parabéns, meu consagrado! Agora você venceu na vida. Acertos: #{acertos}"
     else
-      puts "Não foi dessa vez. Continue trabalhando."
+      puts "Não foi dessa vez. Continue trabalhando. Acertos: #{acertos}"
     end
   end
 end
